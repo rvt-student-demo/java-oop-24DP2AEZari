87e6bfa008605954decf8affae2d3d75be9bbbdc
24DP2AEZari <a240579az@rvt.lv>
2026-06-07 22:32:49 +0300

    Online Shop JM9-2 done!

--- src/main/java/Students/Warehouse.java
+++ src/main/java/Students/Warehouse.java
@@ -1,5 +1,7 @@
 package Students;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 public class Warehouse {
     HashMap<String, Integer> products = new HashMap<>();
@@ -32,42 +34,116 @@ public class Warehouse {
     }
 
 public static class Item {
-            public String product;
-            public int qty;
-            public int unitPrice;
-            HashMap<String, Integer> products = new HashMap<>();
-            HashMap<String, Integer> productPrice = new HashMap<>();
+            private String product;
+            private int qty;
+            private int unitPrice;
 
         public Item(String product, int qty, int unitPrice) {
             this.product = product;
             this.qty = qty;
             this.unitPrice = unitPrice;
-            products.put(product, qty);
-            productPrice.put(product, unitPrice);
         }
+        
         public int totalPrice() {
-            int totalPrice = products.get(product) * productPrice.get(product);
-            return totalPrice;
+            return qty * unitPrice;
         }
+        
         public void increaseQuantity() {
             qty++;
         }
 
         @Override
         public String toString() {
-            return this.product + ":" + qty;
+            return this.product + ": " + qty ;
         }
-    }    
+    }   
+    
+    public static class ShoppingCart {
+        private Map<String, Item> cart;
+        
+        public ShoppingCart() {
+            this.cart = new HashMap<String, Item>();
+        }
+        
+        public void add(String product, int price){
+            if (cart.containsKey(product)) {
+                cart.get(product).increaseQuantity();
+            } else {
+                Item item = new Item(product, 1, price);
+                cart.put(product, item);
+            }
+        }
+        
+        public int price(){
+            int totalPrice = 0;
+            for (Item item : cart.values()) {
+                totalPrice += item.totalPrice();
+            }
+            return totalPrice;
+        }
+        
+        public void print() {
+            for (Item item : cart.values()) {
+                System.out.println(item);
+            }
+        }
+    }
+    public class Store {
+
+    private Warehouse warehouse;
+    private Scanner scanner;
+
+    public Store(Warehouse warehouse, Scanner scanner) {
+        this.warehouse = warehouse;
+        this.scanner = scanner;
+    }
+
+    // the method that handles the customers visit to the store.
+    public void shop(String customer) {
+        ShoppingCart cart = new ShoppingCart();
+        System.out.println("Welcome to the store " + customer);
+        System.out.println("our selection:");
+
+        for (String product : this.warehouse.products()) {
+            System.out.println(product);
+        }
+
+        while (true) {
+            System.out.print("What to put in the cart (press enter to go to the register): ");
+            String product = scanner.nextLine();
+            if (product.isEmpty()) {
+                break;
+            }
+
+            // Add code here that adds the product to the cart,
+            // If there is any in the warehouse, and reduces the stock in the warehouse
+            // Don't touch any of the other code!
+
+            if (warehouse.stock(product) > 0) {
+                if (warehouse.take(product)) {
+                    int productPrice = warehouse.price(product);
+                    cart.add(product, productPrice);
+                }
+            }
+        }
+
+        System.out.println("your shoppingcart contents:");
+        cart.print();
+        System.out.println("total: " + cart.price());
+    }
+}
 
 
-    public static void main(String[] args) {
-        Item item = new Item("milk", 4, 2);
-        Item bread = new Item("bread", 5, 4);
-        System.out.println("an item that contains 4 milks has the total price of " + item.totalPrice());
-        System.out.println("an item that contains 5 bread has the total price of " + bread.totalPrice());
-        System.out.println(item);
-        bread.increaseQuantity();
-        System.out.println(item + " and pricie of bread is " + bread);   
+    public void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        Scanner scanner = new Scanner(System.in);
+    warehouse.addProduct("coffee", 5, 10);
+    warehouse.addProduct("milk", 3, 20);
+    warehouse.addProduct("cream", 2, 55);
+    warehouse.addProduct("bread", 7, 8);
+
+    Store store = new Store(warehouse, scanner);
+    store.shop("John");
 
 }
 }
